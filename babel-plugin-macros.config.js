c12434f9415d5e98ba925c0ac94d93b389650196
Aaron Brager <getaaron@gmail.com>
2023-07-04 17:22:09 -0500

    demo

---
+++ babel-plugin-macros.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twin: {
+    preset: "emotion",
+    styled: "styled-components",
+    config: "tailwind.config.js",
+    format: "auto"
+  }
+};
---
+++ next.config.js
@@ -0,0 +1,5 @@
+const withTwin = require('./withTwin.js')
+
+module.exports = withTwin({
+    reactStrictMode: true,
+})
